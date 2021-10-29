527d41b5baee0dc9da79b95fea71a2fcb7aa618d
Allison Gonçalves <aallison93@gmail.com>
2021-10-29 22:31:24 +0100

    fill only work days

--- src/steps/timesheet/index.ts
+++ src/steps/timesheet/index.ts
@@ -1,6 +1,28 @@
-import { By, Key, WebDriver } from 'selenium-webdriver';
+import { By, Key, WebDriver, WebElement } from 'selenium-webdriver';
 
 import { quickPause } from '../../util/time';
+
+const isWeekDayText = (strongElementText: string): boolean => {
+  return ['Mon', 'Tue', 'Wed', 'Thu', 'Fri']
+    .some(day => strongElementText.includes(day));
+};
+
+const filterWeekDayRows = async (rows: WebElement[]): Promise<WebElement[]> => {
+  const daysToFill = [];
+
+  for (const row of rows) {
+    try {
+      const strongElement = await row.findElement(By.css('.col-xs-2'));
+      const strongElementText = await strongElement.getText();
+
+      if (isWeekDayText(strongElementText)) daysToFill.push(row);
+    } catch (error) {
+      continue; // element not found
+    }
+  }
+
+  return daysToFill;
+};
 
 export const clickOnTimesheetSideMenu = async (driver: WebDriver): Promise<void> => {
   const sideMenu = await driver.findElement(By.id('side-menu'));
@@ -25,30 +47,28 @@ export const clickOnLastTimesheetPeriod = async (driver: WebDriver): Promise<voi
   await quickPause();
 };
 
-export const fillTimesheetForm = async (driver: WebDriver): Promise<void> => {
-  const formTable = await driver.findElement(By.id('timesheetform'));
-
-  const startTimeInputs = await formTable.findElements(By.name('start_time[]'));
+const fillTimesheetFormRow = async (driver: WebElement): Promise<void> => {
+  const startTimeInputs = await driver.findElements(By.name('start_time[]'));
   startTimeInputs.forEach(i => i.sendKeys(
     Key.BACK_SPACE,
     Key.BACK_SPACE,
     Key.BACK_SPACE,
     Key.BACK_SPACE,
     Key.BACK_SPACE,
-    '09:00'
+    '08:45'
   ));
 
-  const endTimeInputs = await formTable.findElements(By.name('end_time[]'));
+  const endTimeInputs = await driver.findElements(By.name('end_time[]'));
   endTimeInputs.forEach(i => i.sendKeys(
     Key.BACK_SPACE,
     Key.BACK_SPACE,
     Key.BACK_SPACE,
     Key.BACK_SPACE,
     Key.BACK_SPACE,
-    '18:00'
+    '17:45'
   ));
 
-  const breakDurationInputs = await formTable.findElements(By.name('break_duration[]'));
+  const breakDurationInputs = await driver.findElements(By.name('break_duration[]'));
   breakDurationInputs.forEach(i => i.sendKeys(
     Key.BACK_SPACE,
     Key.BACK_SPACE,
@@ -56,9 +76,18 @@ export const fillTimesheetForm = async (driver: WebDriver): Promise<void> => {
     '1'
   ));
 
-  const notesInput = await formTable.findElements(By.name('notes[]'));
+  const notesInput = await driver.findElements(By.name('notes[]'));
   notesInput.forEach(i => i.clear());
   notesInput.forEach(i => i.sendKeys('Working on sprint task.'));
 
   await quickPause();
+};
+
+export const fillTimesheetForm = async (driver: WebDriver): Promise<void> => {
+  const formTable = await driver.findElement(By.id('timesheetform'));
+
+  const allRows = await formTable.findElements(By.css('.row'));
+  const fillableRows = await filterWeekDayRows(allRows);
+
+  await Promise.all(fillableRows.map(row => fillTimesheetFormRow(row)));
 };
